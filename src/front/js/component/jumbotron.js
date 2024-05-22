bb552045e1d3d2e7d8b10f369325d23f56bdc780
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-05-22 19:23:32 +0000

    cambios

---
+++ src/front/js/component/jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = () => {
+
+    return(
+        <div className="jumbotron">
+            <h1 className="display-4">ADOPTA UN CHEF</h1>
+            <h3 className="lead">Solo para disfrutones</h3>
+            <hr className="my-4"/>
+            <p>Gastronomía a domicilio: Saborea la excelencia de los mejores chefs en la comodidad de tu hogar</p>
+            <a className="btn btn-primary btn-lg" href="#" role="button">BUSCAR</a>
+        </div>
+    )
+}
+
+export default Jumbotron;
