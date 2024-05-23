be7b5922e7fde2d65c2172eb8b1f8ddf50060820
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-05-23 16:54:18 +0000

    cambios

---
+++ src/front/js/component/buscador.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Buscador = () => {
+
+    return (
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+    )
+}
+
+export default Buscador;
