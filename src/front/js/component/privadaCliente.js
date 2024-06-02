a19bb06f481488b005afa87b2369f13bc69c3046
ManulPrian <156845683+ManulPrian@users.noreply.github.com>
2024-06-02 20:00:30 +0000

    muchos estilos

--- src/front/js/component/privadaCliente.js
+++ src/front/js/component/privadaCliente.js
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from "react-router-dom";
 import Buscador from './buscador';
-import '../../styles/privadaCliente.css'; // Ajusta la ruta de importación del CSS
-
+import { Context } from '../store/appContext';
+import '../../styles/privadaCliente.css';
 
 const PrivadaCliente = () => {
   const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+
+  const usuarioId = 1; 
+  const usuarioEncontrado = store.usuarios.find(usuario => usuario.id === usuarioId);
+  if (usuarioEncontrado) {
+    console.log("Usuario encontrado:", usuarioEncontrado);
+  } else {
+    console.log("No se encontró ningún usuario con el ID:", usuarioId);
+  }
 
   return (
-    <div>
-      <div className="container privadaCliente text-center">
+    <div className='privadaCliente'>
+      <button type="button" className="btn close-button">CERRAR</button>
+      <div className="container text-center">
         <div className="row">
           <div className="col">
             <img 
               src="https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=600" 
               alt="Descripción de la imagen" 
               width="300" 
-              height="360" 
+              height="400" 
             />
           </div>
           <div className="col">
             <h3>Hola !!!</h3>
-            <button type="button" className="btn">EDITAR</button>
-            <button type="button" className="btn">CERRAR</button>
-            <button type="button" className="btn">ELIMINAR</button>
             <br />
-            <p>Nombre</p>
-            <p>Apellido </p>
-            <p>Email </p>
-            <p>DNI </p>
-            <p>Contraseña </p>
-            <p>Pais </p>
-            <p>Población </p>
-            <p>Código Postal </p>
-            <p>Dirección </p>
+            <h6>Nombre</h6>
+            <p>{usuarioEncontrado.nombre}</p>
+            <h6>Apellido</h6>
+            <p>{usuarioEncontrado.apellidos}</p>
+            <h6>Email</h6>
+            <p>{usuarioEncontrado.email}</p>
+            <h6>Telefono</h6>
+            <p>{usuarioEncontrado.telefono}</p>
+            <h6>Dirección</h6>
+            <p>{usuarioEncontrado.direccion}</p>
+            <h6>Población</h6>
+            <p>{usuarioEncontrado.localizacion}</p>
+            <h6>Tipo de Dieta</h6>
+            <p>{usuarioEncontrado.tipo_de_dieta}</p>
+            <h6>Alergias</h6>
+            <p>{usuarioEncontrado.alergias}</p>
           </div>
+        </div>
+        <div className="button-row right-align"> {/* Nuevo contenedor para los botones con clase de alineación derecha */}
+          <button type="button" className="btn">EDITAR</button>
+          <button type="button" className="btn">ELIMINAR</button>
         </div>
       </div>
 
@@ -43,7 +61,7 @@ const PrivadaCliente = () => {
           <div className="col-auto">
             <p className="mb-0">¿Quieres contratar un chef?</p>
           </div>
-            <Buscador />
+          <Buscador />
         </div>
         <h4>Política de privacidad</h4>
         <p>
@@ -52,6 +70,6 @@ const PrivadaCliente = () => {
       </div>
     </div>
   );
-}
+};
 
 export default PrivadaCliente;
--- src/front/js/component/privadaProfesional.js
+++ src/front/js/component/privadaProfesional.js
@@ -4,32 +4,32 @@ import '../../styles/privadaProfesional.css'; // Ajusta la ruta de importación 
 function PrivadaProfesional() {
 
   return (
-    <div>
-      <div className="container privadaProfesional text-center">
+    <div className='privadaProfesional'>
+      <button type="button" className="btn close-button">CERRAR</button>
+      <div className="container text-center">
         <div className="row">
           <div className="col">
             <img 
               src="https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=600" 
               alt="Descripción de la imagen" 
               width="300" 
-              height="360" 
+              height="400" 
             />
           </div>
           <div className="col">
-            <h3>Hola !!!</h3>
-            <button type="button" className="btn">EDITAR</button>
-            <button type="button" className="btn">CERRAR</button>
-            <button type="button" className="btn">ELIMINAR</button>
+            <h3>Hola !!!</h3>            
             <br />
             <p>Nombre </p>
             <p>Apellido </p>
             <p>Email </p>
             <p>DNI </p>
             <p>Contraseña </p>
-            <p>Pais </p>
             <p>Población </p>
-            <p>Código Postal </p>
             <p>Dirección </p>
+          </div>
+          <div className="button-row editar eliminar"> {/* Nuevo contenedor para los botones con clase de alineación derecha */}
+            <button type="button" className="btn">EDITAR</button>
+            <button type="button" className="btn">ELIMINAR</button>
           </div>
         </div>
       </div>
