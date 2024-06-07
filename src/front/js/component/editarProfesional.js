7ad3db100cb46c0a90d13af2100ff2feff201c02
XefDavid <156122937+XefDavid@users.noreply.github.com>
2024-06-07 16:35:46 +0000

    cambioslucas2

--- src/front/js/component/editarProfesional.js
+++ src/front/js/component/editarProfesional.js
@@ -9,10 +9,9 @@ const EditarProfesional = () => {
     const navigate = useNavigate();
 
     const [formData, setFormData] = useState({
-        email: "",
-        password: "",  
         nombre: "",
         apellidos: "",
+        email: "",
         telefono: "",
         localizacion: "",
         direccion: "",
@@ -26,7 +25,7 @@ const EditarProfesional = () => {
         tipo_servicio_chef_comida_de_trabajo: "",
         tipo_servicio_chef_servicio_degustacion: "",
         tipo_servicio_chef_comida_informal: "",
-        tipo_servicio_chef_bacthcooking: "",
+        tipo_servicio_chef_batchcooking: "",
         tipo_servicio_jamonero_corte: "",
         tipo_servicio_jamonero_clase_corte: "",
         tipo_servicio_sumiller_maridaje: "",
@@ -59,7 +58,7 @@ const EditarProfesional = () => {
                 tipo_servicio_chef_comida_de_trabajo: usuario.tipo_servicio_chef_comida_de_trabajo || "",
                 tipo_servicio_chef_servicio_degustacion: usuario.tipo_servicio_chef_servicio_degustacion || "",
                 tipo_servicio_chef_comida_informal: usuario.tipo_servicio_chef_comida_informal || "",
-                tipo_servicio_chef_bacthcooking: usuario.tipo_servicio_chef_bacthcooking || "",
+                tipo_servicio_chef_batchcooking: usuario.tipo_servicio_chef_batchcooking || "",
                 tipo_servicio_jamonero_corte: usuario.tipo_servicio_jamonero_corte || "",
                 tipo_servicio_jamonero_clase_corte: usuario.tipo_servicio_jamonero_clase_corte || "",
                 tipo_servicio_sumiller_maridaje: usuario.tipo_servicio_sumiller_maridaje || "",
@@ -84,13 +83,15 @@ const EditarProfesional = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
+        console.log(formData)
+        
         const dataToSend = {};
         for (let key in formData) {
             if (formData[key] !== "" && formData[key] !== null) {
                 dataToSend[key] = formData[key];
             }
         }
-        actions.editarProfesional(formData)
+        actions.editarProfesional(dataToSend)
             .then(result => {
                 if (result) {
                     window.alert("Profesional editado con éxito");
@@ -101,6 +102,46 @@ const EditarProfesional = () => {
                 console.error("Error al editar el usuario", error);
             });
     };
+
+    const renderChefFields = () => (
+        <>
+            <input type="text" name="tipo_servicio_chef_pica_pica" value={formData.tipo_servicio_chef_pica_pica} placeholder="Servicio Chef Pica Pica" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_chef_taller_de_cocina" value={formData.tipo_servicio_chef_taller_de_cocina} placeholder="Servicio Chef Taller de Cocina" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_chef_comida_de_trabajo" value={formData.tipo_servicio_chef_comida_de_trabajo} placeholder="Servicio Chef Comida de Trabajo" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_chef_servicio_degustacion" value={formData.tipo_servicio_chef_servicio_degustacion} placeholder="Servicio Chef Servicio Degustación" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_chef_comida_informal" value={formData.tipo_servicio_chef_comida_informal} placeholder="Servicio Chef Comida Informal" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_chef_batchcooking" value={formData.tipo_servicio_chef_batchcooking} placeholder="Servicio Chef Batchcooking" onChange={handleChange} />
+        </>
+    );
+
+    const renderSumillerFields = () => (
+        <>
+            <input type="text" name="tipo_servicio_sumiller_maridaje" value={formData.tipo_servicio_sumiller_maridaje} placeholder="Servicio Sumiller Maridaje" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_sumiller_cata" value={formData.tipo_servicio_sumiller_cata} placeholder="Servicio Sumiller Cata" onChange={handleChange} />
+        </>
+    );
+
+    const renderPasteleroFields = () => (
+        <>
+            <input type="text" name="tipo_servicio_pastelero_clase" value={formData.tipo_servicio_pastelero_clase} placeholder="Servicio Pastelero Clase" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_pastelero_desayuno" value={formData.tipo_servicio_pastelero_desayuno} placeholder="Servicio Pastelero Desayuno" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_pastelero_merienda" value={formData.tipo_servicio_pastelero_merienda} placeholder="Servicio Pastelero Merienda" onChange={handleChange} />
+        </>
+    );
+
+    const renderJamoneroFields = () => (
+        <>
+            <input type="text" name="tipo_servicio_jamonero_corte" value={formData.tipo_servicio_jamonero_corte} placeholder="Servicio Jamonero Corte" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_jamonero_clase_corte" value={formData.tipo_servicio_jamonero_clase_corte} placeholder="Servicio Jamonero Clase de Corte" onChange={handleChange} />
+        </>
+    );
+
+    const renderBarmanFields = () => (
+        <>
+            <input type="text" name="tipo_servicio_barman_barra" value={formData.tipo_servicio_barman_barra} placeholder="Servicio Barman Barra" onChange={handleChange} />
+            <input type="text" name="tipo_servicio_barman_clase" value={formData.tipo_servicio_barman_clase} placeholder="Servicio Barman Clase" onChange={handleChange} />
+        </>
+    );
 
     return (
         <div>
@@ -115,50 +156,34 @@ const EditarProfesional = () => {
                         />
                     </div>
                     <div className="col">
-                        <button
-                            type="button"
-                            className="btn btn-primary"
-                            onClick={handleSubmit}
-                        >
-                            Confirmar cambios
-                        </button>                        
-                        <br />
-                        <input type="email" name="email" value={formData.email} placeholder="Email" onChange={handleChange} />
-                        <input type="password" name="password" value={formData.password} placeholder="Password" onChange={handleChange} />
-                        <input type="text" name="nombre" value={formData.nombre} placeholder="Nombre" onChange={handleChange} />
-                        <input type="text" name="apellidos" value={formData.apellidos} placeholder="Apellidos" onChange={handleChange} />
-                        <input type="text" name="telefono" value={formData.telefono} placeholder="Teléfono" onChange={handleChange} />
-                        <input type="text" name="localizacion" value={formData.localizacion} placeholder="Localización" onChange={handleChange} />
-                        <input type="text" name="direccion" value={formData.direccion} placeholder="Dirección" onChange={handleChange} />
-                        <input type="text" name="foto_de_perfil" value={formData.foto_de_perfil} placeholder="Foto de perfil" onChange={handleChange} />
-                        <input type="text" name="descripcion" value={formData.descripcion} placeholder="Descripción" onChange={handleChange} />
-                        <input type="text" name="info_adicional" value={formData.info_adicional} placeholder="Información Adicional" onChange={handleChange} />
-                        <input type="text" name="tipo_de_profesional" value={formData.tipo_de_profesional} placeholder="Tipo de Profesional" onChange={handleChange}/>
-                        <input type="text" name="tipo_de_cocina_especialidad" value={formData.tipo_de_cocina_especialidad} placeholder="Especialidad de Cocina" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_pica_pica" value={formData.tipo_servicio_chef_pica_pica} placeholder="Servicio Chef Pica Pica" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_taller_de_cocina" value={formData.tipo_servicio_chef_taller_de_cocina} placeholder="Servicio Chef Taller de Cocina" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_comida_de_trabajo" value={formData.tipo_servicio_chef_comida_de_trabajo} placeholder="Servicio Chef Comida de Trabajo" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_servicio_degustacion" value={formData.tipo_servicio_chef_servicio_degustacion} placeholder="Servicio Chef Servicio Degustación" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_comida_informal" value={formData.tipo_servicio_chef_comida_informal} placeholder="Servicio Chef Comida Informal" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_chef_bacthcooking" value={formData.tipo_servicio_chef_bacthcooking} placeholder="Servicio Chef Batchcooking" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_jamonero_corte" value={formData.tipo_servicio_jamonero_corte} placeholder="Servicio Jamonero Corte" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_jamonero_clase_corte" value={formData.tipo_servicio_jamonero_clase_corte} placeholder="Servicio Jamonero Clase de Corte" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_sumiller_maridaje" value={formData.tipo_servicio_sumiller_maridaje} placeholder="Servicio Sumiller Maridaje" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_sumiller_cata" value={formData.tipo_servicio_sumiller_cata} placeholder="Servicio Sumiller Cata" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_pastelero_clase" value={formData.tipo_servicio_pastelero_clase} placeholder="Servicio Pastelero Clase" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_pastelero_desayuno" value={formData.tipo_servicio_pastelero_desayuno} placeholder="Servicio Pastelero Desayuno" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_pastelero_merienda" value={formData.tipo_servicio_pastelero_merienda} placeholder="Servicio Pastelero Merienda" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_barman_barra" value={formData.tipo_servicio_barman_barra} placeholder="Servicio Barman Barra" onChange={handleChange} />
-                        <input type="text" name="tipo_servicio_barman_clase" value={formData.tipo_servicio_barman_clase} placeholder="Servicio Barman Clase" onChange={handleChange} />
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" name="email" value={formData.email} placeholder="Email" onChange={handleChange} />
+                            <input type="password" name="password" value={formData.password} placeholder="Password" onChange={handleChange} />
+                            <input type="text" name="nombre" value={formData.nombre} placeholder="Nombre" onChange={handleChange} />
+                            <input type="text" name="apellidos" value={formData.apellidos} placeholder="Apellidos" onChange={handleChange} />
+                            <input type="text" name="telefono" value={formData.telefono} placeholder="Telefono" onChange={handleChange} />
+                            <input type="text" name="localizacion" value={formData.localizacion} placeholder="Localizacion" onChange={handleChange} />
+                            <input type="text" name="direccion" value={formData.direccion} placeholder="Direccion" onChange={handleChange} />
+                            <input type="text" name="foto_de_perfil" value={formData.foto_de_perfil} placeholder="Foto de Perfil" onChange={handleChange} />
+                            <input type="text" name="descripcion" value={formData.descripcion} placeholder="Descripción" onChange={handleChange} />
+                            <input type="text" name="info_adicional" value={formData.info_adicional} placeholder="Información adicional" onChange={handleChange} />
+                            <select name="tipo_de_profesional" value={formData.tipo_de_profesional} onChange={handleChange}>
+                                <option value="">Selecciona el tipo de profesional</option>
+                                <option value="Chef">Chef</option>
+                                <option value="Sumiller">Sumiller</option>
+                                <option value="Pastelero">Pastelero</option>
+                                <option value="Jamonero">Jamonero</option>
+                                <option value="Barman">Barman</option>
+                            </select>
+                            {formData.tipo_de_profesional === "Chef" && renderChefFields()}
+                            {formData.tipo_de_profesional === "Sumiller" && renderSumillerFields()}
+                            {formData.tipo_de_profesional === "Pastelero" && renderPasteleroFields()}
+                            {formData.tipo_de_profesional === "Jamonero" && renderJamoneroFields()}
+                            {formData.tipo_de_profesional === "Barman" && renderBarmanFields()}
+                            <button type="submit">Guardar</button>
+                        </form>
                     </div>
                 </div>
-            </div>
-
-            <div className="container">
-                <h4>Política de privacidad</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis exercitationem iste tempore iure accusantium perspiciatis facere nam nobis pariatur, quod iusto natus totam quaerat deleniti incidunt sint ab ipsam sed.
-                </p>
             </div>
         </div>
     );
