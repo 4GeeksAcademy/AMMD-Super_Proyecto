1101713097965e851120afddb6d61a97896ecf2d
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-06-07 17:45:43 +0000

    boton de ¿hablamos? vinculado a la vista conversacion

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -25,6 +25,7 @@ import { VistaBusquedaProfesionales } from "./pages/vistaBusquedaProfesionales";
 import { VistaEditarUsuario } from "./pages/vistaEditarUsuario";
 import VistaReestablecerContrasena from "./pages/vistaReestablecerContrasena";
 import VistaActualizacionContrasena from "./pages/vistaActualizacionContrasena";
+import { VistaConversacion } from "./pages/vistaConversacion";
 
 
 
@@ -55,6 +56,7 @@ const Layout = () => {
                         <Route element={<VistaEditarUsuario />} path="/editarusuario" />
                         <Route element={<VistaReestablecerContrasena />} path="/reestablecercontrasena" />
                         <Route element={<VistaActualizacionContrasena />} path="/resetpassword/:token" />
+                        <Route element={<VistaConversacion />} path="/conversacion" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
--- src/front/js/pages/vistaContratacionProfesional.js
+++ src/front/js/pages/vistaContratacionProfesional.js
@@ -1,15 +1,28 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from 'react-router-dom';
 
 
 export const VistaContratacionProfesional = () => {
+
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate('/conversacion');
+    };
 
     return (
         <>          
             <div className="container text-center">
                 <div className="row">
                     <div className="col">
-                        <button type="button" className="btn btn-success">¿Hablamos?</button>
+                        <button 
+                            type="button" 
+                            className="btn btn-success"
+                            onClick={handleButtonClick}
+                        >
+                            ¿Hablamos?
+                        </button>
                         <button type="button" className="btn btn-success">Contrátame</button>
                     </div>
                     <div className="col">
