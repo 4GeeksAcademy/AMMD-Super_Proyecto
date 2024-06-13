561c24dd4002c94f55795bbeafd73218c918fd0a
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-06-13 17:10:40 +0000

    privadaProfesional, añado texto antes de que devuelva las modificaciones al editar

--- src/front/js/component/privadaProfesional.js
+++ src/front/js/component/privadaProfesional.js
@@ -77,45 +77,45 @@ const handleCrearOrden = () => {
             {store.profesionales.tipo_de_profesional === "chef" && (
               <>
                 <h6>Tipos de servicio del chef</h6>
-                {store.profesionales.tipo_servicio_chef_pica_pica && <p>{store.profesionales.tipo_servicio_chef_pica_pica}</p>}
-                {store.profesionales.tipo_servicio_chef_taller_de_cocina && <p>{store.profesionales.tipo_servicio_chef_taller_de_cocina}</p>}
-                {store.profesionales.tipo_servicio_chef_comida_de_trabajo && <p>{store.profesionales.tipo_servicio_chef_comida_de_trabajo}</p>}
-                {store.profesionales.tipo_servicio_chef_servicio_degustacion && <p>{store.profesionales.tipo_servicio_chef_servicio_degustacion}</p>}
-                {store.profesionales.tipo_servicio_chef_comida_informal && <p>{store.profesionales.tipo_servicio_chef_comida_informal}</p>}
-                {store.profesionales.tipo_servicio_chef_batchcooking && <p>{store.profesionales.tipo_servicio_chef_batchcooking}</p>} 
+                {store.profesionales.tipo_servicio_chef_pica_pica && <p>Pica-pica: {store.profesionales.tipo_servicio_chef_pica_pica}</p>}
+                {store.profesionales.tipo_servicio_chef_taller_de_cocina && <p>Taller de cocina: {store.profesionales.tipo_servicio_chef_taller_de_cocina}</p>}
+                {store.profesionales.tipo_servicio_chef_comida_de_trabajo && <p>Comida de empresa: {store.profesionales.tipo_servicio_chef_comida_de_trabajo}</p>}
+                {store.profesionales.tipo_servicio_chef_servicio_degustacion && <p>Menú degustación: {store.profesionales.tipo_servicio_chef_servicio_degustacion}</p>}
+                {store.profesionales.tipo_servicio_chef_comida_informal && <p>Comida informal: {store.profesionales.tipo_servicio_chef_comida_informal}</p>}
+                {store.profesionales.tipo_servicio_chef_batchcooking && <p>Batchcooking: {store.profesionales.tipo_servicio_chef_batchcooking}</p>} 
               </>         
             )}
 
             {store.profesionales.tipo_de_profesional === "cortador de jamon" && (
               <>
                 <h6>Tipos de servicio del jamonero</h6>
-                {store.profesionales.tipo_servicio_jamonero_corte && <p>{store.profesionales.tipo_servicio_jamonero_corte}</p>}
-                {store.profesionales.tipo_servicio_jamonero_clase_corte && <p>{store.profesionales.tipo_servicio_jamonero_clase_corte}</p>}
+                {store.profesionales.tipo_servicio_jamonero_corte && <p>Corte de jamón: {store.profesionales.tipo_servicio_jamonero_corte}</p>}
+                {store.profesionales.tipo_servicio_jamonero_clase_corte && <p>Clase de corte de jamón: {store.profesionales.tipo_servicio_jamonero_clase_corte}</p>}
               </>         
             )}
 
             {store.profesionales.tipo_de_profesional === "sumiller" && (
               <>
                 <h6>Tipos de servicio del sumiller</h6>
-                {store.profesionales.tipo_servicio_sumiller_maridaje && <p>{store.profesionales.tipo_servicio_sumiller_maridaje}</p>}
-                {store.profesionales.tipo_servicio_sumiller_cata && <p>{store.profesionales.tipo_servicio_sumiller_cata}</p>}
+                {store.profesionales.tipo_servicio_sumiller_maridaje && <p>Servicio de maridaje: {store.profesionales.tipo_servicio_sumiller_maridaje}</p>}
+                {store.profesionales.tipo_servicio_sumiller_cata && <p>Servicio de cata: {store.profesionales.tipo_servicio_sumiller_cata}</p>}
               </>         
             )}
 
             {store.profesionales.tipo_de_profesional === "pastelero" && (
               <>
                 <h6>Tipos de servicio del pastelero</h6>
-                {store.profesionales.tipo_servicio_pastelero_clase && <p>{store.profesionales.tipo_servicio_pastelero_clase}</p>}
-                {store.profesionales.tipo_servicio_pastelero_desayuno && <p>{store.profesionales.tipo_servicio_pastelero_desayuno}</p>}
-                {store.profesionales.tipo_servicio_pastelero_merienda && <p>{store.profesionales.tipo_servicio_pastelero_merienda}</p>}
+                {store.profesionales.tipo_servicio_pastelero_clase && <p>Clase de pastelería: {store.profesionales.tipo_servicio_pastelero_clase}</p>}
+                {store.profesionales.tipo_servicio_pastelero_desayuno && <p>Desayuno: {store.profesionales.tipo_servicio_pastelero_desayuno}</p>}
+                {store.profesionales.tipo_servicio_pastelero_merienda && <p>Merienda: {store.profesionales.tipo_servicio_pastelero_merienda}</p>}
               </>         
             )}    
 
             {store.profesionales.tipo_de_profesional === "barman" && (
               <>
                 <h6>Tipos de servicio del barman</h6>
-                {store.profesionales.tipo_servicio_barman_barra && <p>{store.profesionales.tipo_servicio_barman_barra}</p>}
-                {store.profesionales.tipo_servicio_barman_clase && <p>{store.profesionales.tipo_servicio_barman_clase}</p>}
+                {store.profesionales.tipo_servicio_barman_barra && <p>Servicio de barra: {store.profesionales.tipo_servicio_barman_barra}</p>}
+                {store.profesionales.tipo_servicio_barman_clase && <p>Clase de barman: {store.profesionales.tipo_servicio_barman_clase}</p>}
               </>         
             )}   
 
